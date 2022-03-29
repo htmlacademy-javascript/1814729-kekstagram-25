95d693fbaa7af9ad3a16d71a69a4f46ca6a9463b
SalabayNatalia <salabay.natalia89@gmail.com>
2022-03-29 18:04:59 +0300

    Правда или действие (часть 1). Правки.

--- js/form.js
+++ js/form.js
@@ -3,11 +3,12 @@ import {MAX_STRING_LENGTH} from './constants.js';
 
 const form = document.querySelector('#upload-select-image');
 const loadPhoto = document.querySelector('#upload-file');
-const FormCloseElement = document.querySelector('#upload-cancel');
+const formCloseElement = document.querySelector('#upload-cancel');
 const loadPhotoOverlay = document.querySelector('.img-upload__overlay');
 const hashtagsField = form.querySelector('.text__hashtags');
 const commentField = form.querySelector('.text__description');
 const body = document.querySelector('body');
+const buttonSubmit = document.querySelector('.img-upload__submit');
 
 const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 
@@ -32,73 +33,107 @@ const onFormKeydown = (evt) => {
 };
 
 const clearForm = () => {
-  form.innerHTML = '';
-
-  pristine.validate();
+  loadPhoto.value = '';
+  hashtagsField.value = '';
+  commentField.value = '';
 };
 
-const validateHashtags = (value) => {
-  if (!value) {
-    return true;
-  }
+function checkHashtagLength () {
+  const uniqueHashtags = hashtagsField.value.toLowerCase().split(' ').filter((item) => item !== '');
+  return uniqueHashtags.length < 6;
+}
 
-  const hashtags = value.toLowerCase().trim().split(' ');
+function checkHashtag () {
+  const uniqueHashtags = hashtagsField.value.toLowerCase().split(' ').filter((item) => item !== '');
+  return uniqueHashtags.every((hashtag) => re.test(hashtag));
+}
 
-  const uniqueHashtags = [];
-
-  for (const hashtag of hashtags) {
-    if (uniqueHashtags.includes(hashtag)) {
-      return false;
+function checkHashtagRepeats () {
+  const uniqueHashtags = hashtagsField.value.toLowerCase().split(' ').filter((item) => item !== '');
+  for (let i = 0; i < uniqueHashtags.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (uniqueHashtags[j].toUpperCase() === uniqueHashtags[i].toUpperCase()) {
+        return false;
+      }
     }
-    if (!re.test(hashtag)) {
-      return false;
-    }
-    uniqueHashtags.push(hashtag);
-  }
-  return uniqueHashtags.length <= 5;
-};
+  } return true;
+}
+
+pristine.addValidator(hashtagsField, checkHashtag, 'хэш-тег начинается с символа #, содержит буквы и числа и не может содержать больше 20 символов');
+pristine.addValidator(hashtagsField, checkHashtagLength, 'нельзя указать больше пяти хэш-тегов');
+pristine.addValidator(hashtagsField, checkHashtagRepeats, 'хэш-теги не должны повторяться (хэш-теги нечувствительны к регистру)');
 
 const validateComment = (string) => isAllowedString(string, MAX_STRING_LENGTH);
+
+pristine.addValidator(commentField, validateComment, 'Длина комментария не более 140 символов');
+
+const activateFormModal = () => {
+  loadPhoto.addEventListener('change', openForm);
+};
+
+const getValidateStatus = () => {
+  const isValid = pristine.validate();
+  if (isValid) {
+    buttonSubmit.disabled = false;
+  } else {
+    buttonSubmit.disabled = true;
+  }
+};
+
+const activateFormValidation = () => {
+  hashtagsField.addEventListener('input', getValidateStatus);
+  commentField.addEventListener('input', getValidateStatus);
+};
+
+const desactivateFormValidation = () => {
+  hashtagsField.removeEventListener('input', getValidateStatus);
+  commentField.removeEventListener('input', getValidateStatus);
+};
 
 function openForm() {
   loadPhotoOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
-
   document.addEventListener('keydown', onPopupEscKeydown);
-}
-
-function closeForm() {
-  loadPhotoOverlay.classList.add('hidden');
-  body.classList.remove('modal-open');
-
-  clearForm();
-
-  document.removeEventListener('keydown', onPopupEscKeydown);
-}
-
-const usForm = () => {
-  pristine.addValidator(hashtagsField, validateHashtags,  'Невалидные хештеги');
-  pristine.addValidator(commentField, validateComment,  'Длина комментария не более 140 символов');
-
-  hashtagsField.addEventListener('keydown', onFormKeydown);
-  commentField.addEventListener('keydown', onFormKeydown);
-
-  loadPhoto.addEventListener('change', () => {
-    openForm();
-  });
-
-  loadPhoto.addEventListener('change', openForm);
-
-  FormCloseElement.addEventListener('click', () => {
+  formCloseElement.addEventListener('click', () => {
     closeForm();
   });
-
-  FormCloseElement.addEventListener('click', closeForm);
 
   form.addEventListener('submit', (evt) => {
     evt.preventDefault();
     pristine.validate();
   });
-};
 
-export default usForm;
+  hashtagsField.addEventListener('keydown', onFormKeydown);
+  commentField.addEventListener('keydown', onFormKeydown);
+
+  activateFormValidation();
+
+  document.addEventListener('click', (evt) => {
+    if (evt.target.closest('.img-upload__form') === null) {
+      form.style.display = 'none';
+    }
+  });
+}
+
+function closeForm() {
+  loadPhotoOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onPopupEscKeydown);
+  formCloseElement.removeEventListener('click', () => {
+    closeForm();
+  });
+
+  form.removeEventListener('submit', (evt) => {
+    evt.preventDefault();
+    pristine.validate();
+  });
+
+  hashtagsField.removeEventListener('keydown', onFormKeydown);
+  commentField.removeEventListener('keydown', onFormKeydown);
+
+  desactivateFormValidation();
+
+  clearForm();
+}
+
+export default activateFormModal;
