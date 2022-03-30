81ab27f84d9aecfd01be89ec1a3edc658f739ffe
SalabayNatalia <salabay.natalia89@gmail.com>
2022-03-30 21:00:40 +0300

    Правда или действие (часть 1). Правки 2.

--- js/form.js
+++ js/form.js
@@ -38,30 +38,38 @@ const clearForm = () => {
   commentField.value = '';
 };
 
-function checkHashtagLength () {
-  const uniqueHashtags = hashtagsField.value.toLowerCase().split(' ').filter((item) => item !== '');
-  return uniqueHashtags.length < 6;
-}
+const hashtags = () => {
+  hashtagsField.value.split(' ').filter((item) => item !== '');
+  return hashtags;
+};
 
-function checkHashtag () {
-  const uniqueHashtags = hashtagsField.value.toLowerCase().split(' ').filter((item) => item !== '');
-  return uniqueHashtags.every((hashtag) => re.test(hashtag));
-}
+const checkHashtagLength = () => {
+  hashtags();
+  return hashtags.length < 6;
+};
 
-function checkHashtagRepeats () {
-  const uniqueHashtags = hashtagsField.value.toLowerCase().split(' ').filter((item) => item !== '');
-  for (let i = 0; i < uniqueHashtags.length; i++) {
+const checkHashtag = () => {
+  hashtags();
+  return hashtags.every((hashtag) => re.test(hashtag));
+};
+
+const checkUniqueHashtag = () => {
+  hashtags();
+  hashtagsField.value.toLowerCase();
+  for (let i = 0; i < hashtags.length; i++) {
     for (let j = 0; j < i; j++) {
-      if (uniqueHashtags[j].toUpperCase() === uniqueHashtags[i].toUpperCase()) {
+      if (hashtags[j].toUpperCase() === hashtags[i].toUpperCase()) {
         return false;
       }
     }
   } return true;
-}
+};
+//   const uniqueHashtags = new Set(hashtagsField);
+//   return uniqueHashtags.size === checkHashtagLength();
 
-pristine.addValidator(hashtagsField, checkHashtag, 'хэш-тег начинается с символа #, содержит буквы и числа и не может содержать больше 20 символов');
-pristine.addValidator(hashtagsField, checkHashtagLength, 'нельзя указать больше пяти хэш-тегов');
-pristine.addValidator(hashtagsField, checkHashtagRepeats, 'хэш-теги не должны повторяться (хэш-теги нечувствительны к регистру)');
+pristine.addValidator(hashtagsField, checkHashtag, 'хэш-тег начинается с символа #, содержит буквы и числа и не может содержит менее 20 символов');
+pristine.addValidator(hashtagsField, checkHashtagLength, 'не более пяти хэш-тегов');
+pristine.addValidator(hashtagsField, checkUniqueHashtag, 'хэш-теги не должны повторяться, хэш-теги нечувствительны к регистру');
 
 const validateComment = (string) => isAllowedString(string, MAX_STRING_LENGTH);
 
@@ -90,49 +98,36 @@ const desactivateFormValidation = () => {
   commentField.removeEventListener('input', getValidateStatus);
 };
 
+const onFormCloseElementClick = () => {
+  closeForm();
+};
+
+const onFormSubmitClick = (evt) => {
+  evt.preventDefault();
+};
+
 function openForm() {
   loadPhotoOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
   document.addEventListener('keydown', onPopupEscKeydown);
-  formCloseElement.addEventListener('click', () => {
-    closeForm();
-  });
-
-  form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    pristine.validate();
-  });
-
+  formCloseElement.addEventListener('click', onFormCloseElementClick);
+  form.addEventListener('submit', onFormSubmitClick);
   hashtagsField.addEventListener('keydown', onFormKeydown);
   commentField.addEventListener('keydown', onFormKeydown);
 
   activateFormValidation();
-
-  document.addEventListener('click', (evt) => {
-    if (evt.target.closest('.img-upload__form') === null) {
-      form.style.display = 'none';
-    }
-  });
 }
 
 function closeForm() {
   loadPhotoOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPopupEscKeydown);
-  formCloseElement.removeEventListener('click', () => {
-    closeForm();
-  });
-
-  form.removeEventListener('submit', (evt) => {
-    evt.preventDefault();
-    pristine.validate();
-  });
-
+  formCloseElement.removeEventListener('click', onFormCloseElementClick);
+  form.removeEventListener('submit', onFormSubmitClick);
   hashtagsField.removeEventListener('keydown', onFormKeydown);
   commentField.removeEventListener('keydown', onFormKeydown);
 
   desactivateFormValidation();
-
   clearForm();
 }
 
